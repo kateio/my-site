02ccdd19e7ccd503fabcb8dfe17ca975eeb7c6d7
Kate Iogansen <kate.iogansen@gmail.com>
2022-02-23 19:54:05 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let headMe = document.querySelector('.head-me');
+let headWork = document.querySelector('.head-work');
+let headContacts = document.querySelector('.head-contacts');
+
+
+headMe.onclick = function(e) {
+  debugger;
+  e.preventDefault();
+  window.scrollTo(0, 1000);
+  console.log('1');
+}
+
+
+
